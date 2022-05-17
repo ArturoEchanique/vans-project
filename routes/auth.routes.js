628817c5cd7df98e6d3a474220e17c6195cbb54e
Gabriela <s.gabriela.bv96@gmail.com>
2022-05-18 01:40:03 +0200

    done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,98 +1,80 @@
-const express = require("express")
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const User = require("../models/User.model")
-const { isAuthenticated } = require("../middlewares/jwt.middleware")
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+const { isAuthenticated } = require("../middlewares/jwt.middleware");
 
+const router = express.Router();
+const saltRounds = 10;
 
-const router = express.Router()
-const saltRounds = 10
-
-router.post('/signup', (req, res, next) => {
-
-    const { email, password, username, imageUrl } = req.body
+router.post("/signup", (req, res, next) => {
+    const { email, password, username, imageUrl } = req.body;
 
     if (password.length < 2) {
-        res.status(400).json({ message: 'Password must have at least 3 characters' })
-        return
+        res.status(400).json({ message: "Password must have at least 3 characters" });
+        return;
     }
 
-    User
-        .findOne({ email })
+    User.findOne({ email })
         .then((foundUser) => {
-
             if (foundUser) {
-                res.status(400).json({ message: "User already exists." })
-                return
+                res.status(400).json({ message: "User already exists." });
+                return;
             }
 
-            const salt = bcrypt.genSaltSync(saltRounds)
-            const hashedPassword = bcrypt.hashSync(password, salt)
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const hashedPassword = bcrypt.hashSync(password, salt);
 
-            return User.create({ email, password: hashedPassword, username, imageUrl })
+            return User.create({ email, password: hashedPassword, username, imageUrl });
         })
         .then((createdUser) => {
-            console.log('----', createdUser)
-            const { email, username, _id } = createdUser
-            const user = { email, username, _id }
+            console.log("----", createdUser);
+            const { email, username, _id } = createdUser;
+            const user = { email, username, _id };
 
-            res.status(201).json({ user })
+            res.status(201).json({ user });
         })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ message: "Internal Server Error" })
-        })
-})
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ message: "Internal Server Error" });
+        });
+});
 
+router.post("/login", (req, res, next) => {
+    const { email, password } = req.body;
 
-
-
-
-router.post('/login', (req, res, next) => {
-
-    const { email, password } = req.body
-
-    if (email === '' || password === '') {
+    if (email === "" || password === "") {
         res.status(400).json({ message: "Provide email and password." });
         return;
     }
 
-    User
-        .findOne({ email })
+    User.findOne({ email })
         .then((foundUser) => {
-
             if (!foundUser) {
-                res.status(401).json({ message: "User not found." })
+                res.status(401).json({ message: "User not found." });
                 return;
             }
 
             if (bcrypt.compareSync(password, foundUser.password)) {
+                const { _id, email, username, role } = foundUser;
 
-                const { _id, email, username, role } = foundUser
+                const payload = { _id, email, username, role };
 
-                const payload = { _id, email, username, role }
-
-                const authToken = jwt.sign(
-                    payload,
-                    process.env.TOKEN_SECRET,
-                    { algorithm: 'HS256', expiresIn: "6h" }
-                )
+                const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, { algorithm: "HS256", expiresIn: "6h" });
 
                 res.status(200).json({ authToken });
-            }
-            else {
+            } else {
                 res.status(401).json({ message: "Unable to authenticate the user" });
             }
         })
-        .catch(err => {
-            console.log(err)
-            res.status(500).json({ message: "Internal Server Error" })
-        })
-})
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({ message: "Internal Server Error" });
+        });
+});
 
+router.get("/verify", isAuthenticated, (req, res, next) => {
+    res.status(200).json(req.payload);
+});
 
-router.get('/verify', isAuthenticated, (req, res, next) => {
-    res.status(200).json(req.payload)
-})
-
-module.exports = router
+module.exports = router;
