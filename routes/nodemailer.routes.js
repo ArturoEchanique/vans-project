94811c1587e26f8ff65a2f5ae3c628c725bb665f
Pablo Andreu <99558817+pandma@users.noreply.github.com>
2022-05-17 13:22:47 +0200

    ...

--- routes/nodemailer.routes.js
+++ routes/nodemailer.routes.js
@@ -2,19 +2,18 @@ const router = require("express").Router()
 const transporter = require("../config/mailing.config");
 
 
-// router.get('/contacto', (req, res) => res.render('pages/contact'))
-
 router.post('/bookingmail', (req, res) => {
 
     const { email, subject, message, name } = req.body
 
-    transporter.sendMail({
-        from: name + ' <vanmeup@project.com>',
-        to: email,
-        subject,
-        text: 'You have a new booking ' + message,
-        html: '<p> You have a message form <b>' + name + '</b> this is the message: ' + message + '</p>'
-    })
+    transporter
+        .sendMail({
+            from: name + ' <vanmeup@project.com>',
+            to: email,
+            subject,
+            text: 'You have a new booking ' + message,
+            html: '<p> You have a message form <b>' + name + '</b> this is the message: ' + message + '</p>'
+        })
         .then(info => res.json(info))
         .catch(error => console.log(error))
 
