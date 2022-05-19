6142763fe1f0925f4fa0a992dfcc9ee158ac83be
Arturo Echanique <arturoechanique@gmail.com>
2022-05-19 19:36:50 +0200

    cors

--- config/index.js
+++ config/index.js
@@ -4,14 +4,31 @@ const logger = require("morgan");
 
 const cookieParser = require("cookie-parser");
 
-const cors = require("cors");
+// const cors = require("cors");
 
 module.exports = (app) => {
   app.set("trust proxy", 1);
 
-  app.use(
-    cors()
-  )
+  // app.use(
+  //   cors()
+  // )
+
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+
   // {
   //   origin: process.env.ORIGIN || "http://localhost:3000",
   //   }
